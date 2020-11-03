670deb70fc64d29f78f6b9d5be3e5a8a1cda8feb
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-11-03 22:15:03 +0200

    2 first charts
    took long enough 🤬

--- client/src/components/charts/SessionsByDays.tsx
+++ client/src/components/charts/SessionsByDays.tsx
@@ -1,52 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 
-interface Props {}
+interface Props {
+  offset: number;
+}
 
-export const SessionsByDays: React.FC = (props: Props) => {
+interface dateData {
+  date: string;
+  count: number;
+}
 
-    const data = [
-        {
-          name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-        },
-        {
-          name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-          name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-        },
-        {
-          name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-          name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-          name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-        },
-        {
-          name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-        },
-      ];
+const SessionsByDays = (props: Props) => {
+  const { offset } = props;
 
-  return ( 
-    <div>   
+  const [newDataArray, setNewDataArray] = useState<dateData[]>([])
+
+  const sortDates = (a:dateData, b:dateData):number => {
+    const dateA:Date = new Date(a.date.split('/').reverse().join('-'));
+    const dateB:Date = new Date(b.date.split('/').reverse().join('-'));
+    console.log(dateA, dateB)
+    return dateA.getTime() - dateB.getTime();
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/events/by-days/${offset}`)
+      .then((res) => res.json())
+      .then((res) => {
+        const sortedArr = res.sort(sortDates);
+        console.log("DATA ARRAY", sortedArr);
+        setNewDataArray([...sortedArr]);
+      });
+  }, [offset]);
+
+  return (
+    <div>
+      {/* <h1>OFFSET: {offset}</h1> */}
         <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
+          width={500}
+          height={300}
+          data={newDataArray}
+          margin={{
+            top: 5, right: 30, left: 20, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="count" stroke="maroon" />
+        </LineChart>
     </div>
-  )
+  );
 };
+
+export default SessionsByDays;
