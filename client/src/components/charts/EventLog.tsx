4dbf809537e2f6dc480d7ef5d61ddd7aaa5a1fd7
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-11-04 16:29:33 +0200

    Adding event log

---
+++ client/src/components/charts/EventLog.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface Props {
+    sorting: string;
+    type: string;
+    browser: string;
+    search: string;
+}
+
+const EventLog = (props: Props) => {
+    const { sorting, type, browser, search } = props;
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EventLog
